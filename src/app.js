5d900c969a9759a74aec4546dec39c653eba3543
Gary K <killengary37@gmail.com>
2025-12-14 06:57:34 -0800

    Implement users CRUD

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import cookieParser from "cookie-parser";
 import authRoutes from "#routes/auth.routes.js";
 import securityMiddleware from '#middleware/security.middleware.js';
+import usersRoutes from "#routes/users.routes.js";
 
 const app = express();
 
@@ -33,6 +34,7 @@ app.get('/api', (req, res) => {
   res.status(200).json({ message: 'Acquisition Api is running' });
 });
 
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
 
 export default app;
